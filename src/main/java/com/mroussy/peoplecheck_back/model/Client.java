23f0db38316e07feba4a5c58309b2fadfe1dea06
John Doe <malachipito@protonmail.com>
2023-02-20 11:39:19 +0100

    Dernière version

--- src/main/java/com/mroussy/peoplecheck_back/model/Client.java
+++ src/main/java/com/mroussy/peoplecheck_back/model/Client.java
@@ -7,35 +7,26 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(length = 50, nullable = false)
     private String fname;
-
     @Column(length = 50, nullable = false)
     private String lname;
-
     @Column(length = 20, nullable = false)
     private String gender;
     @Column(nullable = false)
     private int age;
-
     @Column(length = 20, nullable = false)
     private String phone;
-
     @Column(length = 50, nullable = false)
     private String email;
-
     @Column(length = 50, nullable = false)
     private String address;
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getFname() {
         return fname;
     }
